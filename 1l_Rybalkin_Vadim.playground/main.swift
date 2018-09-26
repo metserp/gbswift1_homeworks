33234d81874db0e46d41768e62a2f5835dc28b36
Vadim <metserp@gmail.com>
2018-09-26 18:25:18 +0300

    Initial Commit

---
+++ 1l_Rybalkin_Vadim.playground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  1l_Rybalkin_Vadim.playground
+//
+//  Created by Вадим Рыбалкин on 26/09/2018.
+//  Copyright © 2018 Вадим Рыбалкин. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
